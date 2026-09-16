52398947d89c15af3441efd7e71b46f73c09f7b1
satalight <114524244+satasatalight@users.noreply.github.com>
2026-09-16 17:57:20 -0400

    Backend: add register endpoint (#4)
    * start Register.php w/ boilerplate copy from Login.php
    
    * update SQL and parameters
    
    * update error checking
    
    * update JSON output format
    
    * move empty field error check before sql execution
    
    * fix duplicate username error
    
    * exit on empty field

---
+++ html/API/Register.php
@@ -0,0 +1,87 @@
+<?php
+    $inData = getRequestInfo();
+
+    /*
+     * This statement uses environment variables for security purposes.
+     * Environment variables are located in etc/contact-manager/environment
+     * API_DB_USER = database account name
+     * API_DB_PASS = database account password
+     */
+    $conn = new mysqli("localhost", getenv('API_DB_USER'), getenv('API_DB_PASS'), "Contact_Manager");
+
+    if(empty($inData["username"]) or empty($inData["password"] or empty($inData["firstname"]) or empty($inData["lastname"])))
+    {
+        returnError("Please fill in all fields.");
+        return;
+    }
+
+    if($conn->connect_error)
+    {
+        returnError($conn->connect_error);
+    }
+    else
+    {
+        /*
+         * Attempts to submit a new user into database table
+         */
+        try {
+            $stmt = $conn->prepare("INSERT INTO `users` VALUES (0, ?, ?, ?, ?)");
+            $stmt->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["username"], $inData["password"]);
+            $status = $stmt->execute();
+        }
+
+        catch (Exception $e) {
+            returnError("The username or password you entered is not valid.");
+            return;
+        }
+
+        returnSuccess();
+
+        $stmt->close();
+        $conn->close();
+    }
+
+    /*
+     * Takes no parameters.
+     * Receives the JSON request and returns it in a format
+     * that the script can make use of.
+     */
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    /*
+     * Takes $obj, which represents an already made string
+     * properly formatted as JSON.
+     * Returns nothing, but echoes
+     * the JSON out to the frontend.
+     */
+    function sendResultJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    /*
+     * Takes $error, which represents an error string of
+     * caller's choice.
+     * Returns nothing, but passes JSON output indicating an error.
+     */
+    function returnError($error)
+    {
+        http_response_code(400);
+        $ret = '{"status":"Error","error":"' . $error . '"}';
+        sendResultJson($ret);
+    }
+
+    /*
+     * Outputs a simple "Success" string in the status field.
+     */
+    function returnSuccess()
+    {
+        http_response_code(200);
+        $ret = '{"status":"Success"}';
+        sendResultJson($ret);
+    }
+?>
