4c1d633dcd285fc6dbafffc5305fe55b7558470e
Yrrabl <barrylucido3@gmail.com>
2026-09-17 21:36:49 -0400

    Added success and error messages for removing a contact, and made it throw an error when attempting to remove a contact without an id (mainly for testing)

--- html/API/RemoveContact.php
+++ html/API/RemoveContact.php
@@ -3,20 +3,33 @@
 	
 	$id = $inData["id"];
 	
-
 	$conn = new mysqli("localhost", getenv('API_DB_USER'), getenv('API_DB_PASS'), "Contact_Manager");
 	if ($conn->connect_error) 
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError($conn->connect_error);
 	} 
 	else
 	{
 		$stmt = $conn->prepare("DELETE FROM contacts WHERE id = ?");
 		$stmt->bind_param("s", $id);
-		$stmt->execute();
+		if ($stmt->execute())
+		{
+			if ($stmt->affected_rows > 0)
+			{
+				returnWithSuccess("Success");
+			}
+			else
+			{
+				returnWithError("Contact not found.");
+			}
+		}
+		else
+		{
+			returnWithError("Error");
+		}
+
 		$stmt->close();
 		$conn->close();
-		returnWithError("");
 	}
 
 	function getRequestInfo()
@@ -24,15 +37,21 @@
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson($obj)
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
+	function returnWithError($err)
 	{
 		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithSuccess($message)
+	{
+		$retValue = '{"success":"' . $message . '"}';
+		sendResultInfoAsJson($retValue);
 	}
 ?>
