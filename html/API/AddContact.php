95911429c17726435c14a0b209e9e286d0d41afc
Yrrabl <barrylucido3@gmail.com>
2026-09-17 21:09:22 -0400

    Added comments and fixed some minor formatting issues

--- html/API/AddContact.php
+++ html/API/AddContact.php
@@ -1,13 +1,21 @@
 <?php
 	$inData = getRequestInfo();
 	
+	/*
+	 *Grabs data needed for a new contact
+	 */
 	$userID = $inData["userID"];
 	$firstname = $inData["firstname"];
 	$lastname = $inData["lastname"];
 	$email = $inData["email"];
 	$phone = $inData["phone"];
 	
-
+	/*
+     * This statement uses environment variables for security purposes.
+     * Environment variables are located in etc/contact-manager/environment
+     * API_DB_USER = database account name
+     * API_DB_PASS = database account password
+     */
 	$conn = new mysqli("localhost", getenv('API_DB_USER'), getenv('API_DB_PASS'), "Contact_Manager");
 	if ($conn->connect_error) 
 	{
@@ -15,6 +23,10 @@
 	} 
 	else
 	{
+	    /*
+	     * Submits a new entry into the contact database
+		 * with information matching the frontend users input
+	     */
 		$stmt = $conn->prepare("INSERT INTO contacts (userID, firstname, lastname, email, phone) VALUES (?, ?, ?, ?, ?)");
 		$stmt->bind_param("sssss", $userID, $firstname, $lastname, $email, $phone);
 		if ($stmt->execute())
@@ -29,26 +41,48 @@
 		$conn->close();
 	}
 
+	/*
+     * Takes no parameters.
+     * Receives the JSON request and returns it in a format
+     * that the script can make use of.
+     */
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
+	/*
+     * Takes $obj, which represents an already made string
+     * properly formatted as JSON.
+     * Returns nothing, but echoes
+     * the JSON out to the frontend.
+     */
 	function sendResultInfoAsJson($obj)
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
+	/*
+     * Takes $error, which represents an error string of
+     * caller's choice.
+     * Returns nothing, but passes JSON output indicating an error.
+     */
 	function returnWithError($err)
 	{
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson($retValue);
 	}
+
+	/*
+     * Takes $message, which represents an success string of
+     * caller's choice.
+     * Represents a successful contact addition.
+     * This finishes constructing JSON to be passed back out.
+	 */
 	function returnWithSuccess($message)
 	{
    		$retValue = '{"success":"' . $message . '"}';
     	sendResultInfoAsJson($retValue);
 	}
-	
 ?>
