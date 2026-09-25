12b85d14532d695897d8d9827b61379e4a3404a8
Adam Sadowsky <adamsadowsky@gmail.com>
2026-09-25 15:26:01 -0400

    Frontend: add contacts page and edits page (#11)
    * Initialize frontend structure and contacts page
    
    * Build contacts page UI with dynamic contact list
    
    * Added searching to contacts page
    
    * Added contacts form UI and editing functionality
    
    * Implemented functionality for adding contacts
    
    * Added contact deletion functionality
    
    * reduce branch scope
    
    * match test user data with backend response
    
    * Revert "reduce branch scope"
    
    This reverts commit d19eaaafdd89009f6bb7432e226daefaf6e703e2.
    
    ---------
    
    Co-authored-by: satalight <satalight.x@gmail.com>
    Co-authored-by: satalight <114524244+satasatalight@users.noreply.github.com>

---
+++ html/js/contacts.js
@@ -0,0 +1,166 @@
+const searchForm = document.getElementById("search-form")
+const searchBar = document.getElementById("search-bar")
+const trash = document.getElementById("trash")
+const add = document.getElementById("add")
+const edit = document.getElementById("edit")
+const editMsg = document.getElementById("edit-msg")
+let editFlag = false
+let deleteFlag = false
+
+const testUser = {
+  "id": 2,
+  "firstname": "Clark",
+  "lastname": "Kent",
+}
+
+sessionStorage.setItem("user", JSON.stringify(testUser))
+
+contacts()
+
+async function contacts() {
+
+    trash.addEventListener("click", () => {
+        if(!deleteFlag) {
+            deleteFlag = true
+            editFlag = false
+            editMsg.textContent = "Select contact to delete"
+            trash.style.backgroundColor = "rgb(189, 188, 188)"
+            edit.style.backgroundColor = "rgb(222, 221, 221)"
+        } else {
+            deleteFlag = false
+            editMsg.textContent = ""
+            trash.style.backgroundColor = "rgb(222, 221, 221)"
+            return
+        }
+    })
+
+    add.addEventListener("click", () => {
+        window.location.href = "contactForm.html?mode=add"
+    })
+
+    edit.addEventListener("click", () => {
+        if(!editFlag) {
+            editFlag = true
+            deleteFlag = false
+            editMsg.textContent = "Select contact to edit"
+            edit.style.backgroundColor = "rgb(189, 188, 188)"
+            trash.style.backgroundColor = "rgb(222, 221, 221)"
+        } else {
+            editFlag = false
+            editMsg.textContent = ""
+            edit.style.backgroundColor = "rgb(222, 221, 221)"
+            return
+        }
+    })
+
+    searchForm.addEventListener("submit", async (e) => {
+        e.preventDefault()
+        const searched = await fetchContacts()
+        const contactsList = document.getElementById("contacts-list")
+
+        if(!searched || searched.results.length === 0) {
+            contactsList.textContent = "No contacts"
+            console.log("No results")
+            return
+        }
+
+        displayContacts(searched)
+    })
+
+    let userContacts = await fetchContacts()
+    
+    if(!userContacts) {
+        return
+    }
+
+  /* let userContacts = { results: [{id: 1, firstname: "John", lastname: "Smith", phone:"123-456-7891", email: "test@gmail.com" },
+        { id: 1, firstname: "Jason", lastname: "Bourne", phone:"123-456-7891", email: "test@gmail.com" },
+        { id: 1, firstname: "Matt", lastname: "Damon", phone:"123-456-7891", email: "test@gmail.com" },
+        { id: 1, firstname: "John", lastname: "Smith", phone:"123-456-7891", email: "test@gmail.com" },
+        { id: 1, firstname: "Jason", lastname: "Bourne", phone:"123-456-7891", email: "test@gmail.com" },
+        { id: 1, firstname: "Matt", lastname: "Damon", phone:"123-456-7891", email: "test@gmail.com" },
+        { id: 1, firstname: "John", lastname: "Smith", phone:"123-456-7891", email: "test@gmail.com" },
+        { id: 1, firstname: "Jason", lastname: "Bourne", phone:"123-456-7891", email: "test@gmail.com" },
+        { id: 1, firstname: "Matt", lastname: "Damon", phone:"123-456-7891", email: "test@gmail.com" }
+    ]}*/
+
+    displayContacts(userContacts)
+     
+}
+
+function displayContacts(userContacts) {
+    const contactsList = document.getElementById("contacts-list")
+    contactsList.innerHTML = ""
+    userContacts.results.forEach((e) => {
+        const contact = document.createElement("div")
+        contact.textContent = e.firstname + " " + e.lastname
+        contactsList.append(contact)
+
+        contact.addEventListener("click", async () => {
+            if(!editFlag && !deleteFlag) {
+                return
+            }
+
+            if(editFlag) {
+                sessionStorage.setItem("editContact", JSON.stringify(e))
+                window.location.href = "contactForm.html?mode=edit"
+            } else if(deleteFlag) {
+                const res = await deleteContact(e.id)
+                window.location.href = "contacts.html"
+
+                if(!res) {
+                    return
+                }
+            }
+        })        
+    });
+}
+
+async function fetchContacts() {
+    try {
+        const user = JSON.parse(sessionStorage.getItem("user"))
+        const res = await fetch("http://68.183.24.52/API/SearchContacts.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                search: searchBar.value,
+                userID: user.id
+            })
+        })
+
+        if(!res.ok) {
+            console.log("response error")
+            return
+        }
+
+        return res.json()
+    } catch(err) {
+        console.log("Contacts could not be retrieved due to " + err)
+    }
+}
+
+async function deleteContact(id) {
+    try {
+        const user = JSON.parse(sessionStorage.getItem("user"))
+        const res = await fetch("http://68.183.24.52/API/RemoveContacts.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                id: id
+            })
+        })
+
+        if(!res.ok) {
+            console.log("response error")
+            return
+        }
+
+        return res.json()
+    } catch(err) {
+        console.log("Contacts could not be deleted due to " + err)
+    }
+}
